d355540b9b060ded318fb793a4a425018efd0cfc
Revan Fazry Huda <147484233+FLuticaL@users.noreply.github.com>
2023-10-23 14:03:55 +0700

    Add files via upload

---
+++ pertemuan1/balok/balok.py
@@ -0,0 +1,17 @@
+#Menghitung Volume dan luas Permukaan Balok
+
+#variabel
+p = 15
+l = 8
+t = 17
+
+#Rumus
+Volume = p * l * t
+LuasPermukaan = 2*((p*l) + (p*t) + (l*t))
+
+#output
+print("panjang : ", p)
+print("lebar : ", l)
+print("tinggi : ", t)
+print("Volume : ", Volume)
+print("LuasPermukaan : ", LuasPermukaan)
