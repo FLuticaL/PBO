6591ec81455ccc26f4d6ed9c547c217824082f73
Revan Fazry Huda <147484233+FLuticaL@users.noreply.github.com>
2023-10-23 14:03:56 +0700

    Add files via upload

---
+++ pertemuan1/kubus/kubus.py
@@ -0,0 +1,12 @@
+#Menghitung Volume dan luas Permukaan kubus
+
+#variabel
+r = 9
+
+#Rumus
+Volume = r**3
+LuasPermukaan = 6*r*r
+#output
+print("rusuk: ", r)
+print("Volume: ", Volume)
+print("LuasPermukaan: ", LuasPermukaan)
