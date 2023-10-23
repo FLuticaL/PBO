2e8bc690ec704e87f177cb6fa088bc7089e9d9b9
Revan Fazry Huda <147484233+FLuticaL@users.noreply.github.com>
2023-10-23 14:03:58 +0700

    Add files via upload

---
+++ pertemuan1/persegi panjang/persegi panjang.py
@@ -0,0 +1,17 @@
+#Menghitung Luas dan keliling persegi panjang
+
+print("Menghitung Luas dan keliling persegi panjang")
+
+#variabel
+panjang = 25
+lebar= 10
+
+#Rumus
+Luas= panjang * lebar
+Keliling= (panjang + lebar) * 2
+
+#output
+print("panjang: ", panjang)
+print("lebar: ", lebar)
+print("Luas: ", Luas)
+print("Keliling: ", Keliling)
