86dbc7163ca767a7f9064cac09fcb308fac4413a
Revan Fazry Huda <147484233+FLuticaL@users.noreply.github.com>
2023-10-23 14:03:58 +0700

    Add files via upload

---
+++ pertemuan1/limas segitiga/limas segitiga.py
@@ -0,0 +1,19 @@
+#Menghitung Volume dan luas Permukaan limas segitiga
+
+#variabel
+a = 10
+T = 16
+t = 12
+s = 21
+
+#Rumus
+Volume = 1/6*a*t*T
+LuasPermukaan = 1/2*a*(t+s+s+s)
+
+#output
+print("alas : ", a)
+print("tinggi limas : ", T)
+print("tinggi segitiga alas : ", t)
+print("sisi tegak : ", s)
+print("Volume : ", Volume)
+print("LuasPermukaan : ", LuasPermukaan)
