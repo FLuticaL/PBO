ecdd08d1c6c6cac993202bb6c7f58d48fb039582
Revan Fazry Huda <147484233+FLuticaL@users.noreply.github.com>
2023-10-23 14:03:56 +0700

    Add files via upload

---
+++ pertemuan1/kerucut/kerucut.py
@@ -0,0 +1,19 @@
+#Menghitung Volume dan luas Permukaan kerucut
+
+#variabel
+r = 7
+pi = 22/7
+t = 24
+s= 25
+
+#Rumus
+Volume = 1/3*pi*r*r*t 
+LuasPermukaan = pi*r*(s + r)
+
+#output
+print("jari-jari: ", r)
+print("pi: ", pi)
+print("tinggi ", t)
+print("sisi miring ", s)
+print("Volume: ", Volume)
+print("LuasPermukaan: ", LuasPermukaan)
