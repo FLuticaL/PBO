2218361e959b4bd733ac7117178961241404b9c6
Revan Fazry Huda <147484233+RevanFazryHuda@users.noreply.github.com>
2023-11-27 15:12:17 +0700

    Delete pertemuan 7/suara.py

--- pertemuan 7/suara.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS 
-from playsound import playsound
-
-mytext = 'Selamat Belajar ya!'
-language = 'ja'
-myobj = gTTS(text=mytext, lang=language, slow=False) 
-
-myobj.save("C:/Users/Revan/Documents/VSCode/PBO.py/pertemuan 5/selamat.mp3") 
-playsound("C:/Users/Revan/Documents/VSCode/PBO.py/pertemuan 5/selamat.mp3", True)
